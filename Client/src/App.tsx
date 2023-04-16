466653deb1ed75601fdedc2827677f14b8232f1a
Nicolas Mateus <61887751+nicoMP@users.noreply.github.com>
2023-04-15 20:39:34 -0400

    Set Up Basic Routes

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Tortas By Andrea</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="bg-zinc-300 h-screen w-screen absolute overflow-scroll">
+      <Routes>
+        <Route path ="/" element={<Home/>}></Route>
+      </Routes>
+      <Routes>
+        <Route path ="/imagenes" element={<Home/>}></Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default App
